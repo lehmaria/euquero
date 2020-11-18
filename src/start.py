cd83aa3de5366dd9344581cb961555056ffe2819
lehmaria <leticiamps@live.com>
2020-11-17 22:47:41 -0300

    atualizei gitignore com .idea

--- src/start.py
+++ src/start.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, Response, jsonify, json
+from flask_restful import Resource, Api, fields, marshal
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db import models
 
 # inicia o projeto flask
 app = Flask(__name__)
+api = Api(app)
 
 # declara a conexao com o banco 
 engine = create_engine('sqlite:///euquero.db', echo=True)
@@ -16,15 +19,27 @@ models.Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-session.add(models.User(email='a@a.a', name='ana', password='123', cpf='123'))
+usuario = models.User(email='a@a.a', name='ana', password='123', cpf='123')
+
+session.add(usuario)
 session.commit()
 
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String,
+    'cpf': fields.String,
+}
+
+# classe que define recurso a ser adicionado
+class HelloWorld(Resource):
+    def get(self):
+        inicio = session.query(models.User).all()
+        all_users = [marshal(user, resource_fields) for user in inicio]
+        return {'data': all_users}
+
 # define a rota
-@app.route('/')
-# funçao que responde a rota
-def home_handler():
-    print(session.query(models.User).all())
-    return ''
+api.add_resource(HelloWorld, '/')
 
 # define a porta e inicializa a aplicação
 app.run(port=5000)
